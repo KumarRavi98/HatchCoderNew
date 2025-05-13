4c7c12ae73e050ed6c3f5dfd8586f9ce4efb3171
KumarRavi98 <ravi.hatchcoders@gmail.com>
2025-05-13 16:57:46 +0530

    Deploy build

--- src/javascript.jsx
+++
@@ -1,9 +0,0 @@
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// // import herosection from "./Components/Hero_section.js";
-// import App from "./App.js";
-
-// ReactDOM.render(<App />, document.getElementById("app"));
-    
